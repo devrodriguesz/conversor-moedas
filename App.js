bbdd27b27408466af96c468d39c6ca97155d694e
devrodriguesz <dev.emersonrodrigues@outlook.com>
2022-08-24 22:54:11 -0300

    Passando pro Picker os objetos selecionaveis

--- App.js
+++ App.js
@@ -1,71 +1,95 @@
-import React,{useEffect, useState}  from 'react';
-import { View, Text, StyleSheet, TextInput, TouchableOpacity } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { View, Text, StyleSheet, TextInput, TouchableOpacity, ActivityIndicator } from 'react-native';
 
 import Picker from './src/components/Picker';
 import api from './src/services/api';
 
-export default function App(){
+export default function App() {
   const [moedas, setMoedas] = useState([]);
   const [loading, setLoading] = useState(true);
 
   const [moedaSelecionada, setMoedaSelecionada] = useState(null);
   const [moedaBValor, setMoedaBValor] = useState(0);
 
-  useEffect(()=>{
-    async function loadMoedas(){
+  useEffect(() => {
+    async function loadMoedas() {
       const response = await api.get('all')
-      console.log(Object.keys(response.data))
+
+      let arrayMoedas = []
+      Object.keys(response.data).map((key) => {
+        arrayMoedas.push({
+          key: key,
+          label: key,
+          value: key
+        })
+      })
+
+      setMoedas(arrayMoedas);
+      setLoading(false);
+
     }
 
     loadMoedas();
   }, []);
 
-  return(
-    <View style={styles.container}>
-      
-      <View style={styles.areaMoeda}>
-        <Text style={styles.titulo}>Selecione sua moeda</Text>
-        <Picker/>
+
+  if (loading) {
+    return (
+      <View style={{ justifyContent: 'center', alignItems: 'center', flex: 1 }}>
+        <ActivityIndicator size={45} color='#ae0222' />
       </View>
+    )
+  } else {
+    return (
+      <View style={styles.container}>
 
-      <View style={styles.areaValor}>
-        <Text style={styles.titulo}>Digite um valor para converter em (R$)</Text>
-        <TextInput
-        placeholder='Ex: 150'
-        style={styles.input}
-        keyboardType="numeric"
-        onChangeText={ (valor)=> setMoedaBValor(valor) }
-        />
+        <View style={styles.areaMoeda}>
+          <Text style={styles.titulo}>Selecione sua moeda</Text>
+          <Picker moedas={moedas} onChange={ (moeda)=> setMoedaSelecionada(moeda) } />
+        </View>
+
+        <View style={styles.areaValor}>
+          <Text style={styles.titulo}>Digite um valor para converter em (R$)</Text>
+          <TextInput
+            placeholder='Ex: 150'
+            style={styles.input}
+            keyboardType="numeric"
+            onChangeText={(valor) => setMoedaBValor(valor)}
+          />
+        </View>
+
+        <TouchableOpacity style={styles.botaoArea}>
+          <Text style={styles.botaoTexto}>Converter</Text>
+        </TouchableOpacity>
+
+        <View style={styles.areaResultado}>
+          <Text style={styles.valorConvertido}>
+            3 USD
+          </Text>
+          <Text style={[styles.valorConvertido, { fontSize: 18, margin: 10 }]}>
+            Corresponde a
+          </Text>
+          <Text style={styles.valorConvertido}>
+            19,90
+          </Text>
+        </View>
+
       </View>
+    )
+  }
 
-      <TouchableOpacity style={styles.botaoArea}>
-        <Text style={styles.botaoTexto}>Converter</Text>
-      </TouchableOpacity>
 
-      <View style={styles.areaResultado}>
-        <Text style={styles.valorConvertido}>
-          3 USD
-        </Text>
-        <Text style={[styles.valorConvertido, {fontSize: 18, margin: 10}]}>
-          Corresponde a
-        </Text>
-        <Text style={styles.valorConvertido}>
-          19,90
-        </Text>
-      </View>
 
-    </View>
-  )
 }
 
 const styles = StyleSheet.create({
-  container:{
-    flex:1 ,
+  container: {
+    flex: 1,
     alignItems: 'center',
     backgroundColor: '#101215',
     paddingTop: 40
   },
-  areaMoeda:{
+  areaMoeda: {
     width: '90%',
     backgroundColor: '#f9f9f9',
     paddingTop: 9,
@@ -73,19 +97,19 @@ const styles = StyleSheet.create({
     borderTopRightRadius: 9,
     marginBottom: 2
   },
-  titulo:{
+  titulo: {
     fontSize: 15,
     color: '#000',
     paddingTop: 5,
     paddingLeft: 5,
   },
-  areaValor:{
+  areaValor: {
     width: '90%',
     backgroundColor: '#f9f9f9',
     paddingBottom: 9,
     paddingTop: 9
   },
-  input:{
+  input: {
     width: '100%',
     padding: 10,
     height: 45,
@@ -93,7 +117,7 @@ const styles = StyleSheet.create({
     marginTop: 8,
     color: '#000'
   },
-  botaoArea:{
+  botaoArea: {
     width: '90%',
     backgroundColor: '#fb4b57',
     height: 45,
@@ -102,12 +126,12 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center'
   },
-  botaoTexto:{
+  botaoTexto: {
     fontSize: 17,
     color: '#FFF',
     fontWeight: 'bold'
   },
-  areaResultado:{
+  areaResultado: {
     width: '90%',
     backgroundColor: '#FFF',
     marginTop: 35,
@@ -115,11 +139,11 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     padding: 25
   },
-  valorConvertido:{
+  valorConvertido: {
     fontSize: 39,
     fontWeight: 'bold',
     color: '#000'
   }
-  
+
 
 })
--- src/components/Picker/index.js
+++ src/components/Picker/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import RNPickerSelect from 'react-native-picker-select';
 
-export default function Picker(){
+export default function Picker(props){
     const placeholder={
         label: 'Selecione uma moeda...',
         value: null,
@@ -12,11 +12,8 @@ export default function Picker(){
     return(
         <RNPickerSelect
         placeholder={placeholder}
-        items={[
-            {key:'1', label: 'USD', value: 'USD'},
-            {key:'2', label: 'EUR', value: 'EUR'},
-        ]}
-        onValueChange={ (valor) => console.log(valor) }
+        items={ props.moedas }
+        onValueChange={ (valor) => props.onChange(valor) }
         style={{
             inputIOS:{
                 fontSize: 20,
